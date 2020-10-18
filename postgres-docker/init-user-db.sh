802ea74f925ef85ec70bcd5bb1c782c1c4261666
root <root@ocp4.example.com>
2020-10-18 08:25:56 -0300

    hide sensitive data on init-user-db.sh

--- postgres-docker/init-user-db.sh
+++ postgres-docker/init-user-db.sh
@@ -3,7 +3,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE ROLE  keycloak WITH ENCRYPTED PASSWORD '1m9a8z4urka';
+  CREATE ROLE  keycloak WITH ENCRYPTED PASSWORD 'censored';
   CREATE DATABASE keycloak OWNER keycloak;
   GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
 EOSQL
